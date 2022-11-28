b24c4c2dfb547951646346e7b601f53e2eb58fbf
unknown <sp40016@gmail.com>
2022-11-28 11:01:49 +0530

    cms folder commite

---
+++ application/logs/log-2022-11-28.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-11-28 10:48:10 --> Severity: Warning --> fsockopen(): php_network_getaddresses: getaddrinfo failed: No such host is known.  C:\xampp\htdocs\cms\system\libraries\Email.php 2069
+ERROR - 2022-11-28 10:48:10 --> Severity: Warning --> fsockopen(): Unable to connect to ssl://smtp_host:465 (php_network_getaddresses: getaddrinfo failed: No such host is known. ) C:\xampp\htdocs\cms\system\libraries\Email.php 2069
